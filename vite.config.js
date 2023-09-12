05ecc3cd5402943bb5738319b58c9529aec937f2
Abdelhak MEKAOUI <mekaouiabdelhak1@gmail.com>
2023-09-12 21:49:07 +0100

    waata deploy awld lklba

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "es2015", // Set the target to ES2015
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Clear manualChunks to enable Vite to bundle modules correctly
-      },
-    },
+  commonjsOptions: {
+    esmExternals: true,
   },
-  base: "/mindmate/",
+  base: "/",
 });
